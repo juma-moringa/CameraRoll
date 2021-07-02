641adee5938ce90b3a26938ecf66c34a377ef851
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-02 17:31:08 +0300

    created the image model and its objects

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image=models.ImageField(upload_to='images/')
+    image_name=models.CharField(max_length=200)
+    description=models.TextField()
+    location=models.ForeignKey()
+    category=models.ForeignKey()
