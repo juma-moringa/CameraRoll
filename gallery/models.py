c187ac7e4a8885aca8eeab24ebe18d6386218a63
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-02 17:44:59 +0300

    created the location and the category models/updated the db

--- gallery/models.py
+++ gallery/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 # Create your models here.
+# 1st class
 class Image(models.Model):
     image=models.ImageField(upload_to='images/')
     image_name=models.CharField(max_length=200)
     description=models.TextField()
     # location=models.ForeignKey()
     # category=models.ForeignKey()
+
+
+
+#2nd class
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+
+#3rd class
+class Category(models.Model):
+    name = models.CharField(max_length=50)    
